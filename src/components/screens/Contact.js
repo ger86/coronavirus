8dadef883c889d85c193718397df1f14c83f36cd
Gerardo Fernández <gefe.86@gmail.com>
2020-03-20 10:57:14 +0100

    feature: added input file and refactor form using useReducer

--- src/components/screens/Contact.js
+++ src/components/screens/Contact.js
@@ -1,46 +1,71 @@
-import React, {useState} from 'react';
+import React, {useReducer, useState} from 'react';
 import FormGroup from 'components/UI/FormGroup';
 import Label from 'components/UI/Label';
 
+const MODIFY_INPUT = 'MODIFY_INPUT';
+
+const initialState = {
+  fullname: '',
+  email: '',
+  message: ''
+};
+
+function reducer(state, action) {
+  if (action.type === MODIFY_INPUT) {
+    return {
+      ...state,
+      [action.field]: action.value
+    };
+  } else {
+    return state;
+  }
+}
+
 const Contact = () => {
-  const [fullname, setFullname] = useState('');
-  const [email, setEmail] = useState('');
-  const [message, setMessage] = useState('');
+  const [state, dispatch] = useReducer(reducer, initialState);
+  const [image, setImage] = useState(null);
 
   function handleSubmit(event) {
     event.preventDefault();
+    const {fullname, email, message} = state;
     console.log(`${fullname} ${email} ${message}`);
+    console.log(image);
   }
 
-  function handleChangeFullname(event) {
-    setFullname(event.target.value);
+  function handleChangeInput(event) {
+    dispatch({
+      type: MODIFY_INPUT,
+      field: event.target.name,
+      value: event.target.value
+    });
   }
 
-  function handleChangeEmail(event) {
-    setEmail(event.target.value);
-  }
-
-  function handleChangeMessage(event) {
-    setMessage(event.target.value);
+  function handleChangeFile(event) {
+    setImage(URL.createObjectURL(event.target.files[0]));
   }
 
   return (
     <form onSubmit={handleSubmit}>
       <FormGroup>
         <Label>Nombre completo:</Label>
-        <input type="text" value={fullname} onChange={handleChangeFullname} />
+        <input name="fullname" type="text" value={state.fullname} onChange={handleChangeInput} />
       </FormGroup>
       <FormGroup>
         <Label>Email:</Label>
-        <input type="text" value={email} onChange={handleChangeEmail} />
+        <input name="email" type="text" value={state.email} onChange={handleChangeInput} />
       </FormGroup>
       <FormGroup>
-        <Label>Message:</Label>
-        <textarea value={message} onChange={handleChangeMessage} />
+        <Label>Mensaje:</Label>
+        <textarea name="message" value={state.message} onChange={handleChangeInput} />
+      </FormGroup>
+      <FormGroup>
+        <Label>Tu foto:</Label>
+        <input type="file" onChange={handleChangeFile} />
+        {image && <img src={image} alt="Imagen" />}
       </FormGroup>
       <input type="submit" value="Enviar" />
     </form>
   );
-}
+};
 
 export default Contact;
